cca9ecf4676fa56df867e91a065cdb9c97a61597
Kazuki Furukawa <khmkazu921@gmail.com>
2021-04-22 22:24:03 +0900

    added svls

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq debug-on-error t)
 
 (package-initialize)
 (setq package-archives
@@ -9,7 +10,7 @@
 ;; window
 ;;
 
-(setq default-frame-alist '((width . 180) (height . 155) (top . 0) (left . 0)))
+(setq default-frame-alist '((top . 0) (left . 550) (width . 150) (height . 155)))
 (setq-default indent-tabs-mode nil)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -17,13 +18,15 @@
 (if window-system (progn
                     (toggle-scroll-bar -1)
                     (setq linum-format "%3d")
-                    (set-frame-parameter (selected-frame) 'alpha '(95 95))
-                    (add-to-list 'default-frame-alist '(alpha 95 95))))
+                    (set-frame-parameter (selected-frame) 'alpha '(100 100))
+                    (add-to-list 'default-frame-alist '(alpha 100 100))))
 
 (set-face-foreground 'linum "gray60")
 (load-theme 'wombat t)
 (if (not window-system) (progn
                           (setq linum-format "%3d ")))
+
+(delete-selection-mode 1)
 
 ;;
 ;; auto-mode-alist
@@ -61,15 +64,15 @@
 ;; company
 ;;
 
-(require 'company)
-(global-company-mode)
-(setq company-transformers '(company-sort-by-backend-importance)) 
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1) 
-(setq company-selection-wrap-around t)
-(setq completion-ignore-case t)
-(setq company-dabbrev-downcase nil)
-(define-key company-active-map [tab] 'company-complete-selection)
+;; (require 'company)
+;; (global-company-mode)
+;; (setq company-transformers '(company-sort-by-backend-importance)) 
+;; (setq company-idle-delay 0)
+;; (setq company-minimum-prefix-length 1) 
+;; (setq company-selection-wrap-around t)
+;; (setq completion-ignore-case t)
+;; (setq company-dabbrev-downcase nil)
+;; (define-key company-active-map [tab] 'company-complete-selection)
 
 ;;
 ;; yatex
@@ -167,13 +170,108 @@
                     :background "#000066")
 
 ;;
+;; use-package
+;;
+
+(require 'use-package)
+
+;;
+;; lsp-mode
+;;
+
+(use-package company
+  :ensure t
+  :defer t
+  :init (global-company-mode t)
+  :config
+  ;; Company Flx adds fuzzy matching to company, powered by the sophisticated
+  ;; sorting heuristics  in =flx=
+  (use-package company-flx
+    :ensure t
+    :after company
+    :init (company-flx-mode t))
+  (use-package company-quickhelp
+    :after company
+    :ensure t
+    ;; :init (company-quickhelp-mode t)
+    :hook (prog-mode . (lambda ()
+                         (when (window-system)
+                           (company-quickhelp-local-mode))))
+    :config
+    (setq company-quickhelp-delay 0.0
+          company-quickhelp-max-lines nil)))
+
+(add-hook 'verilog-mode-hook #'lsp)
+
+(use-package lsp-mode
+  :defer t
+  :ensure t
+  :commands lsp
+  :config
+  (setq lsp-log-io nil
+        lsp-auto-configure t
+        lsp-auto-guess-root t
+        lsp-enable-completion-at-point t
+        lsp-enable-xref t
+        lsp-prefer-flymake nil
+        lsp-use-native-json t
+        lsp-enable-indentation t
+        lsp-response-timeout 10
+        lsp-restart 'auto-restart
+        lsp-keep-workspace-alive t
+        lsp-eldoc-render-all nil
+        lsp-enable-snippet nil
+        lsp-enable-folding t)
+         ; lsp-ui gives us the blue documentation boxes and the sidebar info
+  (use-package lsp-ui
+    :defer t
+    :ensure t
+    :after lsp
+    :commands lsp-ui-mode
+    :config               
+    (setq lsp-ui-sideline-ignore-duplicate t
+          lsp-ui-sideline-delay 0.5
+          lsp-ui-sideline-show-symbol t
+          lsp-ui-sideline-show-hover t
+          lsp-ui-sideline-show-diagnostics t
+          lsp-ui-sideline-show-code-actions t
+          lsp-ui-peek-always-show t
+          lsp-ui-doc-use-childframe t)
+    :bind
+    (:map lsp-ui-mode-map
+          ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+          ([remap xref-find-references] . lsp-ui-peek-find-references))
+    :hook
+    ((lsp-mode . lsp-ui-mode)
+     (lsp-managed-mode . (lambda ()
+                           (setq-local company-backends '(company-capf))))
+     (lsp-after-open . (lambda ()
+                         (lsp-ui-flycheck-enable t)
+                         (lsp-ui-sideline-enable t)
+                         (lsp-ui-imenu-enable t)
+                         (lsp-lens-mode t)
+                         (lsp-ui-peek-enable t)
+                         (lsp-ui-doc-enable t))))
+    )
+  )
+  
+;;
 ;; verilog mode
 ;;
 
-(require 'verilog-mode)
-(autoload 'verilog-mode "verilog-mode" "Verilog mode" t )
-(add-to-list 'company-keywords-alist (cons 'verilog-mode verilog-keywords))
-(add-hook 'verilog-mode-hook '(lambda () (font-look-mode 1)))
+(use-package verilog-mode
+  :defer t
+  :config
+  (require 'lsp)
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("svls"))
+   :major-modes '(verilog-mode)
+   :priority -1
+   ))
+  :hook (verilog-mode . (lambda()
+      (lsp)
+      (flycheck-mode t)
+      (add-to-list 'lsp-language-id-configuration '(verilog-mode . "verilog")))))
 
 (setq verilog-indent-level             4
       verilog-indent-level-module      4
@@ -190,13 +288,26 @@
 ;       verilog-tab-always-indent        t
 ;       verilog-auto-endcomments         t
 ;       verilog-minimum-comment-distance 40
-)
+      )
 
-;;
-;; use-package
-;;
+;; ;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; ;; if you are helm user
+;; (use-package helm-lsp :commands helm-lsp-workspace-symbol)
+;; ;; if you are ivy user
+;;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
 
-(require 'use-package)
+;; optionally if you want to use debugger
+;; (use-package dap-mode)
+;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+
+;; optional if you want which-key integration
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.1))
 
 ;;
 ;; flycheck / verilator
@@ -204,15 +315,8 @@
 
 (use-package flycheck
   :ensure t
-  :init (global-flycheck-mode)
-  :config
-  (setq flycheck-find-checker-executable 'verilog-verilator))
-
-(defun flycheck-verilog-get-files()
-  (save-excursion
-    (goto-char (point-min))
-    (if (re-search-forward "verilog-library-files:( *\"\\([^)]+\\)\" *)" nil t)
-        (split-string (match-string-no-properties 1) "\" *\"") (list))))
+  :defer t
+  :init (global-flycheck-mode t))
 
 ;;
 ;; lisp-mode
@@ -236,61 +340,62 @@
 ;; minimap-mode
 ;;
 
-(if window-system (progn
-(require 'minimap)
-(minimap-mode t); 常に有効にする
-(setq minimap-window-location 'right)
-(setq minimap-update-delay 0)
-(setq minimap-minimum-width 20)
-(setq minimap-highlight-line t)
-(setq minimap-width-fraction 0.0
-      minimap-dedicated-window t)
-;; changing colors
+;; (if window-system
+;;     (progn
+;;       (require 'minimap)
+;;       (minimap-mode t); 常に有効にする
+;;       (setq minimap-window-location 'right)
+;;       (setq minimap-update-delay 0)
+;;       (setq minimap-minimum-width 20)
+;;       (setq minimap-highlight-line t)
+;;       (setq minimap-width-fraction 0.1
+;;             minimap-dedicated-window t)
+;;       ;; changing colors
+;;       (custom-set-faces
+;;        ;; custom-set-faces was added by Custom.
+;;        ;; If you edit it by hand, you could mess it up, so be careful.
+;;        ;; Your init file should contain only one such instance.
+;;        ;; If there is more than one, they won't work right.
+;;        '(minimap-active-region-background ((((background dark)) (:background "gray40")) (t (:background "gray60"))) nil (quote minimap))
+;;        '(minimap-current-line-face ((((background dark)) (:background "gray60")) (t (:background "gray80"))) nil (quote minimap)))
+;;       (set-face-attribute 'region nil :background "#666")
+;;       )
+;;   )
+
+;; ;;
+;; ;; neo-tree
+;; ;;
+
+;; (require 'neotree)
+;; (defun neotree-startup ()
+;;   (interactive)
+;;   (neotree-show)
+;;   (call-interactively 'other-window))
+
+;; (if (daemonp)
+;;     (add-hook 'server-switch-hook #'neotree-startup)
+;;   (add-hook 'after-init-hook #'neotree-startup)
+;;   )
+
+;; (use-package neotree
+;; ;;  :init
+;; ;;  (setq-default neo-keymap-style 'concise)
+;;   :config
+;;   (setq neo-smart-open t)
+;;   (setq neo-create-file-auto-open t)
+;;   (setq-default neo-show-hidden-files t)
+;;   (setq neo-theme 'ascii)
+;;   (bind-key [f10] 'neotree-toggle)
+;;   (bind-key "RET" 'neotree-enter-hide neotree-mode-map)
+;;   (bind-key "a" 'neotree-hidden-file-toggle neotree-mode-map)
+;;   (bind-key "<left>" 'neotree-select-up-node neotree-mode-map)
+;;   (bind-key "<right>" 'neotree-change-root neotree-mode-map)
+;;   )
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(minimap-active-region-background ((((background dark)) (:background "gray40")) (t (:background "gray60"))) nil (quote minimap))
- '(minimap-current-line-face ((((background dark)) (:background "gray60")) (t (:background "gray80"))) nil (quote minimap)))
-(set-face-attribute 'region nil :background "#666")
-))
-
-;;
-;; neo-tree
-;;
-
-(require 'neotree)
-(defun neotree-startup ()
-  (interactive)
-  (neotree-show)
-  (call-interactively 'other-window))
-
-(if (daemonp)
-    (add-hook 'server-switch-hook #'neotree-startup)
-  (add-hook 'after-init-hook #'neotree-startup)
-)
-(use-package neotree
-;;  :init
-;;  (setq-default neo-keymap-style 'concise)
-  :config
-  (setq neo-smart-open t)
-  (setq neo-create-file-auto-open t)
-  (setq-default neo-show-hidden-files t)
-  (setq neo-theme 'ascii)
-  (bind-key [f10] 'neotree-toggle)
-  (bind-key "RET" 'neotree-enter-hide neotree-mode-map)
-  (bind-key "a" 'neotree-hidden-file-toggle neotree-mode-map)
-  (bind-key "<left>" 'neotree-select-up-node neotree-mode-map)
-  (bind-key "<right>" 'neotree-change-root neotree-mode-map)
-  )
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (cuda-mode yatex yasnippet use-package powerline neotree minimap flycheck company))))
-
+ '(minimap-active-region-background ((((background dark)) (:background "gray40")) (t (:background "gray60"))) nil 'minimap)
+ '(minimap-current-line-face ((((background dark)) (:background "gray60")) (t (:background "gray80"))) nil 'minimap))
