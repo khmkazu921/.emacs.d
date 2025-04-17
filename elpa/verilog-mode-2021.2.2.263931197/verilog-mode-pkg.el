312ee19b497911451becee53b0394d8bd27bdf55
Kazuki Furukawa <13583600+khmkazu921@users.noreply.github.com>
2025-04-17 22:40:28 +0900

    Delete elpa directory

--- elpa/verilog-mode-2021.2.2.263931197/verilog-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from verilog-mode.el  -*- no-byte-compile: t -*-
-(define-package "verilog-mode" "2021.2.2.263931197" "major mode for editing verilog source in Emacs" 'nil :keywords '("languages") :authors '(("Michael McNamara" . "mac@verilog.com") ("Wilson Snyder" . "wsnyder@wsnyder.org")) :maintainer '("Michael McNamara" . "mac@verilog.com") :url "https://www.veripool.org")
